a7ebf32046dd932f609eb0871e0ebc24dc8f89a5
Korbinian Polk <mail@korbinianpolk.eu>
2024-07-20 15:46:27 +0200

    add team

--- footer.php
+++ footer.php
@@ -1,10 +1,6 @@
 
 <footer class="section section--footer">
-	<div class="module module--widgets">
-		<ul class="widgets">
-			<?php dynamic_sidebar( 'widgets-footer' ); ?>
-		</ul>
-	</div>
+	
 	<div class="module module--nav-footer">
 		<nav class="nav-footer">
 			<?php wp_nav_menu(
@@ -16,14 +12,10 @@
 			?>
 		</nav>
 	</div>
-	<div class="module module--contactbar">
-		<div class="contactbar">
-			<address>
-				<b>Johannes Baumann IT-Dienstleistungen</b>
-				<span class="has-icon has-icon--location">Marktplatz 8 83530 Schnaitsee</span>
-				<span class="has-icon has-icon--phone"><a href="tel:+4980742609860">+49 8074 2609860</a></span>
-			</address>
-		</div>
+	<div class="module module--widgets">
+		<ul class="widgets">
+			<?php dynamic_sidebar( 'widgets-footer' ); ?>
+		</ul>
 	</div>
 
 </footer>
